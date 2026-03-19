d39dcd449ce4e67fb54c053932b4a7138f60cd90
Pradeep-gif-hub <pawasthi063@gmail.com>
2026-03-19 12:57:02 +0530

    create my_first_node.py

---
+++ my_first_node_package.py
@@ -0,0 +1,18 @@
+import rclpy
+from rclpy.node import Node
+
+class Mynode(Node){
+    def __init__(self):
+        super().__init__('my_first_node')
+        self.get_logger().info('Hello ROS2')
+}
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = Mynode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+if __name__ == '__main__':   
+    main()    
+    
